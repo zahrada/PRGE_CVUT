a46ac7c674ae97fbbd00cfb2ad40ae451ec6ce05
zahrada <32935470+zahrada@users.noreply.github.com>
2018-12-12 16:20:16 +0100

    update

--- GRAV/norm_high.m
+++ GRAV/norm_high.m
@@ -10,7 +10,7 @@ format long
 
 % for plot must do proces_gravimetr for each one dataset
 % compute fayodove redukce
-high_first_niv = 655.702756; % vyska pocatku nivelacniho poradu
+high_first_niv = 613.175; % vyska pocatku nivelacniho poradu
 first_point = 980938.593; %mGal cb 3408,01 vyska pocatku gravimetrickeho mereni
 
 variable_gravimetr_usa=0.1175;
@@ -46,7 +46,10 @@ afternoon_rus = proces_gravimetr(data,variable_gravimetr_rus,first_point);
 % only !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ours measure by points of
 % VPN
 measure_g = (morning_usa+morning_rus+afternoon_usa+afternoon_rus)/4;
-measure_g_ours = measure_g(8:10,:);
+measure_g_ours = measure_g(8:11,:);
+measure_g_ours(1,2) = (morning_usa(8,2)+afternoon_usa(8,2)+afternoon_rus(8,2))/3;
+
+measure_g_ours = [measure_g_ours; 0,980938.593,0,0; 0,980938.593,0,0];
 
 %import points
 filename = 'body.txt';
@@ -69,35 +72,56 @@ for i = 1:m
     %gama(i) = 978032.7*(1+0.0053024*sin(points_decimal(i,2))*sin(points_decimal(i,2))- 0.0000058*sin(2*points_decimal(i,2))*sin(2*points_decimal(i,2)));
     % fayova anomalie
     g_fayah(i) = measure_g_ours(i,2)+0.3086*points_decimal(i,4)-gama(i);
+    g_bug(i) = g_fayah(i) - 0.119 * points_decimal(i,4)+14;
+    
+    points_decimal(i,4);
 end
 %korekce z tihovzch anomalii
 
 for i = 1: m-1
     %korekce z tihovzch anomalii
-    korekce_tih_anom(i) = 0.0010193 * ((g_fayah(i+1)-g_fayah(i))/2) * (points_decimal(i+1,4)-points_decimal(i,4));
+    korekce_tih_anom(i) = 0.0010193 * ((g_fayah(i+1)+g_fayah(i))/2) * (points_decimal(i+1,4)-points_decimal(i,4));
     % normalni ortometricka korekce
-    norm_orto_korekce(i) = -0.0000254* (points_decimal(i,4)+(points_decimal(i+1,4)-points_decimal(i,4))/2 ) * ((points(i+1,4)-points(i,4))/2);
+    norm_orto_korekce(i) = -0.0000254* (points_decimal(i,4)+points_decimal(i+1,4))/2 * ((points(i+1,3)*60+points(i+1,4)-points(i,3)*60-points(i,4)));
     %normalni prevyseni
     high_norm(i) = (points_decimal(i+1,4)-points_decimal(i,4))+korekce_tih_anom(i)/1000+norm_orto_korekce(i)/1000;
     %normalni vysky
 end
+
+
 high_point(1) = high_first_niv;
-high_point(2) = high_first_niv-high_norm(2); %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11POZOR PORADI BODU 
-high_point(3) = high_point(2)-high_norm(1); %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11POZOR PORADI BODU 
-high_point;
+high_point(2) = high_first_niv-high_norm(m-1); %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11POZOR PORADI BODU 
+high_point(3) = high_point(2)-high_norm(m-2); %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11POZOR PORADI BODU 
+high_point(4) = high_point(3)-high_norm(m-3);
+high_point(5) = high_point(4)-high_norm(m-4);
+high_point(6) = high_point(5)-high_norm(m-5);
+
+for i = 1:6
+high_p(1) = high_point(6);
+high_p(2) = high_point(5);
+high_p(3) = high_point(4);
+high_p(4) = high_point(3);
+high_p(5) = high_point(2);
+high_p(6) = high_point(1);  
+end
+
 
 fprintf('===============================================================================================================\n')
 fprintf('                                               Gravimetr                                                       \n')
 fprintf('---------------------------------------------------------------------------------------------------------------\n')
+fprintf('morning_usa      morning_rus     afternoon_usa   afternoon_rus \n')
+for i = 1:length(morning_usa)
+    fprintf('%5.1f\t\t %5.1f\t\t %5.1f\t\t %5.1f\t\t\n',morning_usa(i,2), morning_rus(i,2), afternoon_usa(i,2),afternoon_rus(i,2) )
+end
 
-fprintf('Vyska_VPN        Normalni_tize   Merena_tize     Fayova_anomalie \n')
+fprintf('Vyska_VPN        Normalni_tize   Merena_tize     Fayova_anom  Bougerova_anom\n')
 for i = 1:m
-    fprintf('%5.6f\t\t %5.3f\t\t %5.3f\t\t %5.3f\t\t\n',points_decimal(i,4), gama(i), measure_g_ours(i,2), g_fayah(i) )
+    fprintf('%5.3f\t\t %5.0f\t\t %5.0f\t\t %5.0f\t\t  %5.0f\t\t\n',points_decimal(i,4), gama(i), measure_g_ours(i,2), g_fayah(i), g_bug(i) )
 end
 
 fprintf('Prevyseni_VPN    Kor_tih_anom    Norm_orto_kor   Norm_prevyseni  Norm_vyska \n')
 for i = 1:m-1
-    fprintf('%5.6f\t\t %5.5f\t\t %5.5f\t\t %5.6f\t\t %5.6f\t\t \n',points_decimal(i+1,4)-points_decimal(i,4),korekce_tih_anom(i),  norm_orto_korekce(i), high_norm(i), high_point(i+1) )
+    fprintf('%5.6f\t\t %5.3f\t\t\t %5.3f\t\t\t %5.6f\t\t %5.3f\t\t \n',points_decimal(i+1,4)-points_decimal(i,4),korekce_tih_anom(i),  norm_orto_korekce(i), high_norm(i), high_p(i) )
 end
 
 
